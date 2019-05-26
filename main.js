489d77f371473b4e3ec512a124219b5e4036cdfd
Kevin <dev7kevin@gmail.com>
2019-05-26 03:08:12 -0400

    Fixed Bug:  Connection lost.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var mysql = require('mysql');
+/*
 var db = mysql.createConnection({
   host: 'us-cdbr-iron-east-02.cleardb.net',
   user: 'b146bb005f0339',
@@ -10,7 +11,36 @@ var db = mysql.createConnection({
   database: 'heroku_3492c0ae37ea14e'
 });
 db.connect();
+*/
+
+var db_config = {
+  host: 'us-cdbr-iron-east-02.cleardb.net',
+  user: 'b146bb005f0339',
+  password: '3860ba3e',
+  database: 'heroku_3492c0ae37ea14e'
+};
+var db;
+function handleDisconnect(){
+  db = mysql.createConnection(db_config);
+  db.connect(function(err){
+    if(err){
+      console.log('error when connection to db', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+
+  db.on('error', function(err){
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+      handleDisconnect();
+    }else{
+      throw err;
+    }
+  });
+}
  
+handleDisconnect();
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
